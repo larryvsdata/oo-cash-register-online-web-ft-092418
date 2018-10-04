12f0a62a443a37471dccecf87515a458f93d4651
larryvsdata <ermanbekaroglu@gmail.com>
2018-10-04 01:24:52 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,12 +3,12 @@
 
 
 def CashRegister
-  attr_accessor :total
+  attr_accessor :total, :discount
   
   
   def initialize(discount=0.0)
     @total=0
-    
+    @discount=discount
   end
   
   def total
